8674c621058d3b626d5075f6ad87fff40c378c0f
Umair Ahmed Imran Butt <119656634+UmairAhmedImran@users.noreply.github.com>
2024-02-07 01:25:21 +0500

    Add files via upload
    Starting react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function Profile() {
+  return (
+  <img
+  src="https://i.imgur.com/MK3eW3As.jpg"
+  alt="Katherine Johnson"
+/>
+  );
+};
+
+export default function Gallary() {
+  return (
+    <section>
+      <h1>Amazing Scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+};
